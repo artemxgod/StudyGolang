cd27270446a024a572b493b946de00ef31412ed8
artem karapetyan <artemka111777@mail.ru>
2021-09-18 19:57:15 +0300

    Add all array&slice tasks

--- basics/array.go
+++ basics/array.go
@@ -3,17 +3,18 @@ package main
 import "fmt"
 //formatted output
 func main(){
-	// var num float64
+	var num float64
 
-	// fmt.Scan(&num)
-	// if num <= 0 {
-	// 	fmt.Printf("число %2.2f не подходит", num)
-	// }else if num > 10000{
-	// 	fmt.Printf("%e", num)
-	// }else { 
-	// 	fmt.Printf("%.4f", num*num)
-	// }
+	fmt.Scan(&num)
+	if num <= 0 {
+		fmt.Printf("число %2.2f не подходит", num)
+	}else if num > 10000{
+		fmt.Printf("%e", num)
+	}else { 
+		fmt.Printf("%.4f", num*num)
+	}
 
+	//Array and slice
 	//	Task 1
 	var(workArray [10]uint8 
 		idx1, idx2 int
@@ -32,5 +33,64 @@ func main(){
 		fmt.Print(workArray[i], " ")
 	}
 
+	//Task 2 - slices
+	var	N,value int
+	
+	baseslice := make([]int, 4, 4)
+	fmt.Scan(&N)
+	for i := 0; i<N;i++{
+		if i < 4{
+			fmt.Scan(&baseslice[i])
+		}else{
+			fmt.Scan(&value)
+			baseslice = append(baseslice, value)
+		}
+	}
+	fmt.Print(baseslice[3])
+	
+	//Task 3 - find and output max elem
+	array := [5]int{}
+	var a int
+	for i:=0; i < 5; i++{
+		fmt.Scan(&a)
+		array[i] = a
+	}
+	var max int
 
+	max = -1000
+
+	for i:=0;i < 5; i++{
+		if max < array[i]{
+			max = array[i]
+		}
+	}
+	fmt.Print(max)
+	
+	//Task 4 - output even elems
+	var( N int
+		baseArray [100]int)
+	
+	fmt.Scan(&N)
+	for i:=0; i < N; i++{
+		fmt.Scan(&baseArray[i])
+	}
+	for i:=0; i < N; i++{
+		if(i % 2 == 0){
+			fmt.Print(baseArray[i], " ")
+		}
+	}
+
+	//Task 5 - amount of positive numbers
+	var (N,cnt int
+		array [100]int)
+
+		cnt = 0
+		fmt.Scan(&N)
+		for i:=0; i < N; i++{
+			fmt.Scan(&array[i])
+			if(array[i] >= 0){
+				cnt++
+			}
+		}
+		fmt.Print(cnt)
 }
